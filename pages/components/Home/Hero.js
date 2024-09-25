cd3bfe0f7e1bb60aee5dd225383c793be7f53f0d
dreayoung <youngmandrea@gmail.com>
2024-09-25 13:11:03 -0400

    retry images

--- pages/components/Home/Hero.js
+++ pages/components/Home/Hero.js
@@ -3,30 +3,29 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import ImageCarousel from '../image-carousel';
 
-const images = [
-  '/food/foodimg1.jpeg',
-  '/food/foodimg2.jpeg',
-  '/food/foodimg3.jpeg',
-  '/food/foodimg5.jpeg',
-  '/food/foodimg1.jpeg',
-  '/food/foodimg2.jpeg',
-  '/food/foodimg3.jpeg',
-  '/food/foodimg5.jpeg',
-];
-
-const images2 = [
-  '/food/foodimg9.jpeg',
-  '/food/foodimg8.jpeg',
-  '/food/foodimg7.gif',
-  '/food/foodimg4.jpeg',
-  '/food/foodimg9.jpeg',
-  '/food/foodimg8.jpeg',
-  '/food/foodimg7.gif',
-  '/food/foodimg4.jpeg',
-];
-
 export default function Hero() {
-  const router = useRouter();
+  const images = [
+    '/food/foodimg1.jpeg',
+    '/food/foodimg2.jpeg',
+    '/food/foodimg3.jpeg',
+    '/food/foodimg5.jpeg',
+    '/food/foodimg1.jpeg',
+    '/food/foodimg2.jpeg',
+    '/food/foodimg3.jpeg',
+    '/food/foodimg5.jpeg',
+  ];
+
+  const images2 = [
+    '/food/foodimg9.jpeg',
+    '/food/foodimg8.jpeg',
+    '/food/foodimg7.gif',
+    '/food/foodimg4.jpeg',
+    '/food/foodimg9.jpeg',
+    '/food/foodimg8.jpeg',
+    '/food/foodimg7.gif',
+    '/food/foodimg4.jpeg',
+  ];
+  
   return (
     <>
       <div className="grid grid-cols-2 md:grid-cols-3 h-screen overflow-hidden">
